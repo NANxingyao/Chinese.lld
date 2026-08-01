0e2c3ce833c17d4386be270b8c1014b603d69b34
Ryan <166373439+NANxingyao@users.noreply.github.com>
2026-08-01 10:15:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ logger = logging.getLogger(__name__)
 
 # 页面配置
 st.set_page_config(
-    page_title="汉语词类隶属度检测划类",
+    page_title="基于大语言模型的汉语隶属度检测划类平台
+    ——Chinese Affinity Detection and Classification Platform Based on LLM",
     page_icon="📰",
     layout="wide",
     initial_sidebar_state="collapsed",
